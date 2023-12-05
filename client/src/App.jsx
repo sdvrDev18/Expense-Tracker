0951de24cd4090ffec7a656ae81504f2dc346261
Sachdev R <sdvr.dev.18@gmail.com>
2023-12-06 01:08:49 +0530

    Changed App file to show same bg image for all routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/home/home.jsx";
 import Login from "./components/user/login.jsx";
-import Dashboard from "./components/home/Dashboard.jsx";
+import Dashboard from "./components/dashboard/dashboard.jsx";
+import LoginOptions from "./components/user/loginOptions.jsx";
+import user_icon from "./assets/icons/user_icon.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="main-container">
+        <header className="header-main-container">
+          <div className="header-main-filler-section"></div>
+          <h1 className="header-main-caption">EXPENSE TRACKER</h1>
+          <img
+            className="header-main-icon"
+            src={user_icon}
+            alt="user"
+            height="50"
+            width="50"
+          />
+        </header>
+        <Routes>
+          <Route path="/" element={<LoginOptions />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
